98c80d4b6bdc39ac0194565b0f3e21831d4e71d5
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-07-31 22:07:58 -0500

    Remove Build Bundles section of install.sh - ensure symlinks are specified fully

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ for item in .*
 do
     if [ $item != '.' ] && [ $item != '..' ]; then
         echo "Linking $item to $HOME/$item"
-        ln -sf `pwd`/$item $HOME/
+        ln -sf `pwd`/$item $HOME/$item
     fi
 done
 
@@ -26,14 +26,6 @@ print_message 'Updating environment...' \
     'You can ignore the "bind: warning: line editing not enabled"' \
     'Simply run "source ~/.bash_profile" once installation is complete to get those binds!'
 source $HOME/.bash_profile
-
-# Build vim plugins
-print_message 'Building vim plugins...'
-if hash rake 2>/dev/null; then
-    cd `pwd`/.vim/bundle/command-t && rake make
-else
-    echo 'Ruby/Rake not installed. Unable to build command-t plugin'
-fi
 
 print_message 'dotfiles installed!' \
     'Now run osx.sh if this is on a Mac, and you're golden!'
