60df0b84c87717acd9672765172293adfe210af4
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-12-06 20:56:55 -0600

    Update prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,32 +1,46 @@
-function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+function fish_prompt
+	set_color yellow
+   printf '%s' (whoami)
+   set_color normal
+   printf ' at '
 
-  # User
-  set_color $fish_color_user
-  echo -n (whoami)
-  set_color normal
+   set_color magenta
+   printf '%s' (hostname|cut -d . -f 1)
+   set_color normal
+   printf ' in '
 
-  echo -n '@'
+   set_color $fish_color_cwd
+   printf '%s' (prompt_pwd)
+   set_color normal
 
-  # Host
-  set_color $fish_color_host
-  echo -n (hostname -s)
-  set_color normal
+   set -l cyan (set_color -o cyan)
+   set -l yellow (set_color -o yellow)
+   set -l red (set_color -o red)
+   set -l blue (set_color -o blue)
+   set -l normal (set_color normal)
 
-  echo -n ':'
+   function _git_branch_name
+      echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||') 
+   end 
+   function _is_git_dirty
+      echo (git status -s --ignore-submodules=dirty ^/dev/null) 
+   end 
+   if [ (_git_branch_name) ]
+      set -l git_branch $red(_git_branch_name)
+      set git_info "$blue git:($git_branch$blue)" 
+   end
+   if [ (_is_git_dirty) ]
+      set -l dirty "$yellow ✗"
+      set git_info "$git_info$dirty"
+   end
 
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (prompt_pwd)
-  set_color normal
+   printf $git_info
 
-  __terlar_git_prompt
-  echo
-
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  end
-
-  echo -n '➤ '
-  set_color normal
+   # Line 2
+   echo
+   if test $VIRTUAL_ENV
+       printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+   end
+   printf '↪ '
+   set_color normal
 end
