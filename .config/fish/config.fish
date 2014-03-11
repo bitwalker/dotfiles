91e0c969ec1e2a7a4be2d86d1801bd8b7c4565f0
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-03-11 00:27:07 -0500

    Add kiex to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,13 @@
 # Load paths
 set BINPATH /usr/local/bin /usr/local/sbin /usr/local/Cellar /usr/bin /usr/sbin /bin
 set RVM ~/.rvm/bin
+set KIEX ~/.kiex/bin
 set GNUUTILS /usr/local/opt/coreutils/libexec/gnubin
 set GNUMAN /usr/local/opt/coreutils/libexec/gnuman
 set HEROKU /usr/local/heroku/bin
 set GOPATH ~/Repositories/Go
 
-set -x PATH $GNUUTILS $BINPATH $HEROKU $RVM $PATH
+set -x PATH $GNUUTILS $BINPATH $HEROKU $RVM $KIEX $PATH
 set -x MANPATH $GNUMAN $MANPATH
 
 set -x EDITOR "vim"
