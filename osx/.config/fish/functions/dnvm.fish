1d9362e55234932caf9c5c81403ea25f34ee99c0
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-07-06 11:57:40 -0500

    Fix for dnvm script

--- osx/.config/fish/functions/dnvm.fish
+++ osx/.config/fish/functions/dnvm.fish
@@ -1,4 +1,4 @@
 function dnvm -d 'DNX Version Manager'
-  bash -c 'source /usr/local/bin/dnvm.sh; dnvm "$@"; status=$?; exit $status' $argv
+  bash -c 'source /usr/local/bin/dnvm.sh; dnvm "$@"; status=$?; exit $status' nil $argv
 end
 
