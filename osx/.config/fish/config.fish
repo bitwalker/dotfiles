c1d11a0c218e21c90c5fbb61f510ad671a768957
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-08-19 09:54:11 -0500

    Update config.fish

--- osx/.config/fish/config.fish
+++ osx/.config/fish/config.fish
@@ -12,8 +12,11 @@ end
 #
 # In order for this theme to render correctly, you will need a
 # [Powerline-patched font](https://gist.github.com/1595572).
+# set -g theme_display_user yes
+# set -g default_user your_normal_user
 
 set -g theme_display_user yes
+set -g default_user Schoens
 set -g current_bg NONE
 set segment_separator \uE0B0
 set right_segment_separator \uE0B0
@@ -23,7 +26,7 @@ set right_segment_separator \uE0B0
 
 set -g __fish_git_prompt_showdirtystate 'yes'
 set -g __fish_git_prompt_char_dirtystate '±'
-set -g __fish_git_prompt_char_cleanstate ''
+set -g __fish_git_prompt_char_cleanstate '✓'
 
 function parse_git_dirty
   set -l submodule_syntax
@@ -89,12 +92,6 @@ end
 # Theme components
 # ===========================
 
-function prompt_virtual_env -d "Display Python virtual environment"
-  if test "$VIRTUAL_ENV"
-    prompt_segment white black (basename $VIRTUAL_ENV)
-  end
-end
-
 function prompt_user -d "Display actual user if different from $default_user"
   if [ "$theme_display_user" = "yes" ]
     if [ "$USER" != "$default_user" -o -n "$SSH_CLIENT" ]
@@ -106,6 +103,33 @@ end
 
 function prompt_dir -d "Display the actual directory"
   prompt_segment blue black (prompt_pwd)
+end
+
+function prompt_virtual_env -d "Display Python virtual environment"
+  if test "$VIRTUAL_ENV"
+    prompt_segment white black (basename $VIRTUAL_ENV)
+  end
+end
+
+function prompt_git -d "Display the actual git state"
+  set -l ref
+  set -l dirty
+  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    set dirty (parse_git_dirty)
+    set ref (command git symbolic-ref HEAD 2> /dev/null)
+    set ref (command git symbolic-ref HEAD 2> /dev/null)
+    if [ $status -gt 0 ]
+      set -l branch (command git show-ref --head -s --abbrev |head -n1 2> /dev/null)
+      set ref "➦ $branch "
+    end
+    set branch_symbol \uE0A0
+    set -l branch (echo $ref | sed  "s-refs/heads/-$branch_symbol -")
+    if [ "$dirty" != "" ]
+      prompt_segment yellow black "$branch $dirty"
+    else
+      prompt_segment green black "$branch $dirty"
+    end
+  end
 end
 
 function prompt_hg -d "Display mercurial state"
@@ -127,26 +151,33 @@ function prompt_hg -d "Display mercurial state"
   end
 end
 
-
-function prompt_git -d "Display the actual git state"
+function prompt_svn -d "Display the current svn state"
   set -l ref
-  set -l dirty
-  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    set dirty (parse_git_dirty)
-    set ref (command git symbolic-ref HEAD 2> /dev/null)
-    set ref (command git symbolic-ref HEAD 2> /dev/null)
-    if [ $status -gt 0 ]
-      set -l branch (command git show-ref --head -s --abbrev |head -n1 2> /dev/null)
-      set ref "➦ $branch "
-    end
-    set branch_symbol \uE0A0
-    set -l branch (echo $ref | sed  "s-refs/heads/-$branch_symbol -")
-    if [ "$dirty" != "" ]
-      prompt_segment yellow black "$branch $dirty"
-    else
-      prompt_segment green black "$branch $dirty"
+  if type svn >/dev/null 2>&1
+    if command svn ls . >/dev/null 2>&1
+      set branch (svn_get_branch)
+      set branch_symbol \uE0A0
+      set revision (svn_get_revision)
+      prompt_segment green black "$branch_symbol $branch:$revision"
     end
   end
+end
+
+function svn_get_branch -d "get the current branch name"
+  svn info 2> /dev/null | awk -F/ \
+      '/^URL:/ { \
+        for (i=0; i<=NF; i++) { \
+          if ($i == "branches" || $i == "tags" ) { \
+            print $(i+1); \
+            break;\
+          }; \
+          if ($i == "trunk") { print $i; break; } \
+        } \
+      }'
+end
+
+function svn_get_revision -d "get the current revision number"
+  svn info 2> /dev/null | sed -n 's/Revision:\ //p'
 end
 
 function prompt_status -d "the symbols for a non zero exit status, root and background jobs"
@@ -173,10 +204,11 @@ end
 function fish_prompt
   set -g RETVAL $status
   prompt_status
-  prompt_virtual_env
   prompt_user
   prompt_dir
-  prompt_hg
+  #prompt_virtual_env
   prompt_git
+  #prompt_hg
+  #prompt_svn
   prompt_finish
 end
