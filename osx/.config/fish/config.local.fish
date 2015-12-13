d650aa239179b7c79380d30ba50c41e6173115df
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-12-12 19:09:52 -0600

    Update fish config

--- osx/.config/fish/config.local.fish
+++ osx/.config/fish/config.local.fish
@@ -1,5 +1,5 @@
 # Local variables
-set BINPATH /usr/local/bin /usr/local/sbin /usr/local/Cellar /usr/bin /usr/sbin /bin
+set BINPATH /usr/local/bin /usr/local/games/bin /usr/local/sbin /usr/local/Cellar /usr/bin /usr/sbin /bin
 set RVM ~/.rvm/bin
 set GNUUTILS /usr/local/opt/coreutils/libexec/gnubin
 set GNUMAN /usr/local/opt/coreutils/libexec/gnuman
@@ -7,12 +7,10 @@ set HEROKU /usr/local/heroku/bin
 set EXENV ~/.exenv/bin ~/.exenv/shims
 
 # Exports
-set -x PATH $GNUUTILS $BINPATH $HEROKU $RVM $EXENV $PATH
+set -gx NVM_DIR ~/.nvm
+set -x PATH $GNUUTILS $BINPATH $HEROKU $RVM $EXENV $NVM_DIR $PATH
 set -x MANPATH $GNUMAN /usr/share/man
 set -x EDITOR "vim"
-
-# Aliases
-alias ls "ls -lAGHksp"
 
 # Init RVM
 rvm >/dev/null
---
+++ osx/.config/fish/functions/ls.fish
@@ -0,0 +1,3 @@
+function ls --description 'List contents of directory'
+	command ls -AFGHhl $argv
+end
