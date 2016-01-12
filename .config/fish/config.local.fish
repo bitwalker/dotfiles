3c148ba663cffaca686e47cb9e7e2537656cef26
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-01-12 14:56:09 -0600

    Update fish config

--- .config/fish/config.local.fish
+++ .config/fish/config.local.fish
@@ -1,17 +1,13 @@
 # Local variables
-set BINPATH /usr/local/bin /usr/local/sbin /usr/local/Cellar /usr/bin /usr/sbin /bin
-set RVM ~/.rvm/bin
+set NPM_BIN node_modules/.bin
 set EXENV ~/.exenv/bin ~/.exenv/shims
+set RBENV ~/.rbenv/bin ~/.rbenv/shims
 
 # Exports
-set -gx NVM_DIR ~/.nvm
 set -x GOPATH ~
-set -x PATH $BINPATH $RVM $EXENV $NVM_DIR $GOPATH/bin $PATH
+set -x PATH $EXENV $RBENV $NPM_BIN $GOPATH/bin /usr/local/bin $PATH
 set -x MANPATH /usr/local/share/man /usr/share/man
 set -x EDITOR "vim"
-
-# Init RVM
-rvm >/dev/null
 
 # Init exenv
 exenv rehash >/dev/null
