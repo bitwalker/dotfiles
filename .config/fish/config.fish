e13c6fddbffd6d242a4d5ad5b3c83b219fd964a6
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-01-13 22:04:37 -0600

    Add fisherman, update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,5 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH /Users/paulschoenfelder/.local/share/omf
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /Users/paulschoenfelder/.config/omf
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
+set fisher_home ~/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
 
 source ~/.config/fish/config.local.fish
--- .config/fish/config.local.fish
+++ .config/fish/config.local.fish
@@ -2,14 +2,16 @@
 set NPM_BIN node_modules/.bin
 set EXENV ~/.exenv/bin ~/.exenv/shims
 set RBENV ~/.rbenv/bin ~/.rbenv/shims
+set N_PREFIX $HOME/n
 
 # Exports
 set -x GOPATH ~
-set -x PATH $EXENV $RBENV $NPM_BIN $GOPATH/bin /usr/local/bin $PATH
+set -x PATH $N_PREFIX/bin $EXENV $RBENV $NPM_BIN $GOPATH/bin /usr/local/bin $PATH
 set -x MANPATH /usr/local/share/man /usr/share/man
 set -x EDITOR "vim"
 
 # Init exenv
 exenv rehash >/dev/null
+
 
 eval (direnv hook fish)
