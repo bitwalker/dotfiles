5bfdfcea9ab8da9fc2948a5707985a001777c7b4
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-01-27 11:35:34 -0600

    Update fish config

--- .config/fisherman/functions/fish_right_prompt.fish
+++ .config/fisherman/functions/fish_right_prompt.fish
@@ -1,5 +1,5 @@
-#!/usr/bin/env fish
-
-# Removes right prompt
 function fish_right_prompt
+  set -l cyan (set_color green)
+  set -l normal (set_color normal)
+  printf (date "+$cyan%H:%M:%S$normal")
 end
