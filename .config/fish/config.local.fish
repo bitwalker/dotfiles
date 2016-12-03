240160b12c9640b69cc002528291e104dbb46cd2
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-12-03 15:25:15 -0600

    Use asdf for version management

--- .config/fish/config.local.fish
+++ .config/fish/config.local.fish
@@ -1,17 +1,18 @@
 # Local variables
 set NPM_BIN node_modules/.bin
 set N_PREFIX $HOME/n
+set CARGO_BIN $HOME/.cargo/bin
 
 # Exports
 #bass source ~/.nix-profile/etc/profile.d/nix.sh
 
 set -x GOPATH ~
-set -x PATH $N_PREFIX/bin $NPM_BIN $GOPATH/bin /usr/local/bin /usr/local/sbin $PATH
+set -x PATH $N_PREFIX/bin $NPM_BIN $GOPATH/bin $CARGO_BIN /usr/local/bin /usr/local/sbin $PATH
 set -x MANPATH /usr/local/share/man /usr/share/man
 set -x EDITOR "vim"
 
-# Init erlang
-#source ~/erlang/19.1/activate.fish
+# Init package managers
+source ~/.asdf/asdf.fish
 
 # OPAM configuration
 . /Users/paulschoenfelder/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
