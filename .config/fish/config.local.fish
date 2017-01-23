07dc45e6be2b0deba2a1ba295978eca9ed27bb8a
Paul Schoenfelder <paulschoenfelder@gmail.com>
2017-01-23 09:48:20 -0800

    Update fish config

--- .config/fish/config.local.fish
+++ .config/fish/config.local.fish
@@ -1,13 +1,12 @@
 # Local variables
 set NPM_BIN node_modules/.bin
-set N_PREFIX $HOME/n
 set CARGO_BIN $HOME/.cargo/bin
 
 # Exports
 #bass source ~/.nix-profile/etc/profile.d/nix.sh
 
 set -x GOPATH ~
-set -x PATH $N_PREFIX/bin $NPM_BIN $GOPATH/bin $CARGO_BIN /usr/local/bin /usr/local/sbin $PATH
+set -x PATH $NPM_BIN $GOPATH/bin $CARGO_BIN /usr/local/bin /usr/local/sbin $PATH
 set -x MANPATH /usr/local/share/man /usr/share/man
 set -x EDITOR "vim"
 
