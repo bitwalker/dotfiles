ec720510af6315a687fa079e22ac577e67319bc8
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-04-05 13:56:10 -0500

    [installer] add doom remote for easy updates

--- install.sh
+++ install.sh
@@ -41,6 +41,9 @@ else
     if [ ! -d ~/.emacs.d ]; then
         echo "Cloning Emacs configuration.."
         git clone git@github.com:bitwalker/doom-emacs ~/.emacs.d
+        pushd ~/.emacs.d
+        git remote add hlissner git@github.com:hlissner/doom-emacs.git
+        popd
         echo "Linking init.el.."
         ln -sf "$CWD/init.el" "$HOME/.emacs.d/init.el"
     fi;
