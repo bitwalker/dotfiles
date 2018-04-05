33adcf1f6b8f0b3c33b39d7a819bd22859f94d65
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-04-05 14:03:13 -0500

    [emacs] add more keybindings

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -11,4 +11,27 @@
   :when (memq window-system '(mac ns x))
   :config (exec-path-from-shell-copy-env "PATH"))
 
-(setq alchemist-key-command-prefix (kbd doom-localleader-key))
+;; Seems to kick in during *any* evil mode, which obviously isn't ideal with SPC as leader
+;;(setq alchemist-key-command-prefix (kbd doom-localleader-key))
+(after! alchemist
+  (map! :map elixir-mode-map
+        :nv "SPC m" alchemist-mode-keymap))
+
+;; Extend projectile keybindings to provide easy way to kill project buffers
+(after! projectile
+  (map! :map projectile-mode-map
+        (:leader
+          (:prefix "p"
+            :desc "Kill project buffers" :nv "k" #'projectile-kill-buffers
+            :desc "Search project with rg" :nv "s" #'counsel-projectile-rg
+            :desc "Switch to project buffer" :nv "b" #'counsel-projectile-switch-to-buffer
+            :desc "Switch to project buffer or file" :nv "SPC" #'counsel-projectile))))
+
+;; Provide keybind to easily change theme
+(map!
+  :after evil
+  (:leader
+    (:prefix "t"
+      :desc "Change theme" :nv "t" #'counsel-load-theme)
+    (:prefix "TAB"
+      :desc "Rename workspace" :nv "r" #'+workspace/rename)))
