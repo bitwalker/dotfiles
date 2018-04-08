6b3fc5e024b8b502d7d8080dc323dd8ed6026e33
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-04-08 14:31:22 -0500

    [installer] refactoring, cleanup, and minor additions
    - Ensure some prerequisite directories are created
    - Clean up script a bit
    - Automate more of Emacs setup
    - Conditionally run components only when they are known to be present
    - Install iTerm2 preferences
    - Auto-update ASDF
    - Install Python utils

--- install.sh
+++ install.sh
@@ -2,55 +2,61 @@
 
 set -e
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
 CWD=$(pwd)
 
-function doIt() {
-  find . \
-       -maxdepth 1 \
-         \! \( -name ".git" \
-               -or -name "emacs" \
-               -or -name "init.el" \
-               -or -name "init" \
-               -or -name "install.sh" \
-               -or -name "Brewfile" \
-               -or -name "requirements.txt" \
-               -or -name "." \
-               -or -name ".#*" \) \
-        -execdir ln -sf "$CWD/{}" "$HOME/{}" \;
-  mkdir -p ~/.config/nvim
-  echo "Creating vim config"
-  ln -sf "$CWD/.vimrc" "$HOME/.config/nvim/init.vim"
-  ln -sf "$CWD/.vimrc" "$HOME/.vim/init.vim"
-  echo "Creating source dirs"
-  mkdir -p ~/bin;
-  mkdir -p ~/src;
-  echo "Sourcing bash profile"
-  touch ~/.bash_profile && source ~/.bash_profile
-  return 0
+# Create prereqs
+mkdir -p ~/bin
+mkdir -p ~/.config/nvim
+mkdir -p ~/src/github.com/bitwalker
+
+printf "# Syncing to home folder...\n"
+function doSync() {
+    find . \
+        -maxdepth 1 \
+        \! \( -name ".git" \
+              -or -name ".DS_Store" \
+              -or -name "emacs" \
+              -or -name "init" \
+              -or -name "install.sh" \
+              -or -name "Brewfile" \
+              -or -name "*.txt" \
+              -or -name "*.md" \
+              -or -name "." \
+              -or -name ".#*" \) \
+              -execdir ln -sf "$CWD/{}" "$HOME/{}" \;
+    ln -sf ~/.vimrc ~/.config/nvim/init.vim
+    ln -sf ~/.vimrc ~/.config/.vim/init.vim
+    return 0
 }
-
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt;
+	doSync
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  echo "";
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt;
-    if [ ! -d ~/.emacs.d ]; then
-        printf "Installing Emacs config.."
-        git clone git@github.com:bitwalker/doom-emacs ~/.emacs.d
-        pushd ~/.emacs.d
-        git remote add hlissner git@github.com:hlissner/doom-emacs.git
-        popd
-        printf "done!\n"
-    fi;
-  fi;
-fi;
-unset doIt;
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	    doSync
+    else
+        printf "Aborted.\n\n"
+        exit 0
+	fi
+fi
+unset doSync
 
-echo "Your dotfiles have been synchronized!"
+# Update shell environment based on profile
+source ~/.bash_profile
+
+if [ ! -d ~/.emacs.d ]; then
+    printf "Installing Emacs config.."
+    git clone git@github.com:bitwalker/doom-emacs ~/.emacs.d
+    pushd ~/.emacs.d
+    git remote add hlissner git@github.com:hlissner/doom-emacs.git
+    popd
+    printf "done!\n"
+fi
+
+echo "dotfiles have been synchronized!"
 
 # Install tmux plugins
 read -p "Install tmux plugins? (y/n) " -n 1;
@@ -62,21 +68,28 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi;
 
-read -p "Install XCode Command Line Tools? (y/n) " -n 1;
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Running installer.."
-  xcode-select --install
-fi;
+# XCode
+if which xcode-select >/dev/null; then
+    read -p "Install XCode Command Line Tools? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "Running installer.."
+        xcode-select --install
+    fi
+fi
 
-read -p "Install Homebrew packages? (y/n) " -n 1;
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Running homebrew..."
-  . Brewfile
-fi;
+# Homebrew
+if which brew >/dev/null; then
+    read -p "Install Homebrew packages? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        . $CWD/Brewfile
+    fi
+else
+    printf "\nHomebrew not installed! Skipping package installation..\n\n"
+fi
 
-# Requires that Emacs is installed
+# Emacs
 if which emacs >/dev/null; then
     echo "Compiling Emacs configuration.."
     pushd ~/.emacs.d
@@ -87,23 +100,49 @@ else
     echo "Skipping Emacs configuration, 'which emacs' failed with non-zero exit"
 fi
 
-read -p "Install iTerm2 color schemes? (y/n) " -n 1;
+# iTerm
+read -p "Install iTerm2 preferences? (y/n) " -n 1;
 echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Fetching iTerm2 color schemes..."
-    git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/src/iTerm2-Color-Schemes
-fi;
+    if [ ! -d ~/src/iTerm2-Color-Schemes ]; then
+        git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/src/iTerm2-Color-Schemes
+    else
+        pushd ~/src/iTerm2-Color-Schemes
+        git pull origin master
+        popd
+    fi
+    open ~/src/iTerm2-Color-Schemes/schemes/Glacier.itermcolors
+    open ~/src/iTerm2-Color-Schemes/schemes/Hybrid.itermcolors
+    open ~/src/iTerm2-Color-Schemes/schemes/Hardcore.itermcolors
+    if which defaults >/dev/null; then
+        defaults read $CWD/com.googlecode.iterm2.plist
+    fi
+fi
 
+# ASDF
 read -p "Install asdf version manager? (y/n) " -n 1;
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Installing version manager..."
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.0
+    if [ ! -d ~/.asdf ]; then
+        git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+    else
+        pushd ~/.asdf
+        git fetch origin
+        popd
+    fi
+    # Update to latest version
+    pushd ~/.asdf
+    __latest_asdf=$(git tag -l | tail -n 1)
+    git checkout $__latest_asdf
+    popd
     source ~/.asdf/asdf.sh
     echo "Adding Erlang, Elixir, Go plugins"
     asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
     asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
     asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-fi;
+fi
+
+echo "Installing Python-based utilities.."
+pip install -r $CWD/requirements.txt
 
 echo ""
 echo "All done!"
